69a25909aa9b1d96d83eeccce18ab5741be10b6f
bosh44 <bosh44@gmail.com>
2017-12-07 13:19:39 +0200

    Improved guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
-numofdir=$(ls | wc -w );
+
+function getGuess {
+  read guess;
+  regex=[^0-9];
+  while [[ $guess =~ $regex ]] || [[ -z $guess ]]
+  do
+    echo 'ERROR: You must enter a number.'
+    echo '       (You have entered "'$guess'")';
+    echo '       Try again:';
+    read guess;
+  done
+  return;
+}
+
+numofdir=$(ls | wc -w);
 echo 'Guess how many files are there in the current directory?';
-read guess;
-regex=[^0-9];
-while [[ $guess =~ $regex ]] || [[ -z $guess ]]
-do
-echo 'ERROR: You must enter a number.'
-echo '       (You have entered "'$guess'")';
-read guess;
-done
+getGuess;
 while [[ $guess != $numofdir ]]
 do
   if [[ $guess -lt $numofdir ]]
   then
     echo 'Too low.... Try again:';
-    read guess;
+    getGuess;
   else
     echo 'Too high... Try again:';
-    read guess;
+    getGuess;
   fi
 done
 echo 'That is right!!!';
+
